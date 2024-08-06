14f3e81dd25fab954aa023300cafc5f3b78d6807
TomokiNakada <tomoki.nakada.331@gmail.com>
2024-08-06 21:05:00 +0900

    最初のコミット

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("/Users/nakadatomoki/project/my-docusaurus/node_modules/infima/dist/css/default/default.css"),
+  require("/Users/nakadatomoki/project/my-docusaurus/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/Users/nakadatomoki/project/my-docusaurus/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/Users/nakadatomoki/project/my-docusaurus/src/css/custom.css"),
+];
